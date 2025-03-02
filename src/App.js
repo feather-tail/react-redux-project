d8bca0030c445a05c5a1ebb65af281b76ec634ea
feather-tail <imeliaslake@gmail.com>
2025-03-03 02:42:37 +0300

    add tasks

--- src/App.js
+++ src/App.js
@@ -1,20 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { increment, decrement, incrementByAmount } from './store/counterSlice';
+import { addTask, removeTask } from './store/tasksSlice';
 
 function App() {
-  const dispatch = useDispatch();
-  const count = useSelector((state) => state.counter.value);
+    const dispatch = useDispatch();
+    const count = useSelector((state) => state.counter.value);
 
-  return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Redux Toolkit Counter</h1>
-      <h2>{count}</h2>
-      <button onClick={() => dispatch(increment())}>+1</button>
-      <button onClick={() => dispatch(decrement())}>-1</button>
-      <button onClick={() => dispatch(incrementByAmount(5))}>+5</button>
-    </div>
-  );
+    const tasks = useSelector((state) => state.tasks.tasks);
+    const [taskText, setTaskText] = useState('');
+
+    console.log('Current tasks from store:', tasks);
+
+    const addNewTask = () => {
+        const newTask = { id: Date.now(), text: taskText };
+        console.log('Adding task:', newTask);
+        dispatch(addTask(newTask));
+        setTaskText('');
+    };
+
+    const removeTaskById = (id) => {
+        dispatch(removeTask({ id }));
+    };
+
+    return (
+        <div style={{ textAlign: 'center', marginTop: '50px' }}>
+            <h1>Redux Toolkit Counter</h1>
+            <h2>{count}</h2>
+            <button onClick={() => dispatch(increment())}>+1</button>
+            <button onClick={() => dispatch(decrement())}>-1</button>
+            <button onClick={() => dispatch(incrementByAmount(5))}>+5</button>
+
+            <h2>Task Manager</h2>
+            <input
+                type="text"
+                value={taskText}
+                onChange={(e) => setTaskText(e.target.value)}
+                placeholder="Add new task"
+            />
+            <button onClick={addNewTask}>Add Task</button>
+
+            <ul>
+                {tasks.map((task) => (
+                    <li key={task.id}>
+                        {task.text}
+                        <button onClick={() => removeTaskById(task.id)}>Remove</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from './counterSlice';
+import tasksReducer from './tasksSlice';
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+    reducer: {
+        counter: counterReducer,
+        tasks: tasksReducer,
+    },
 });
-
